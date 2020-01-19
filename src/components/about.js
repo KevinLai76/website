a465893defcf87f6ef01f1d406ecdc30ea941eda
Kevin Lai <kevinlai76@gmail.com>
2020-01-19 17:40:24 -0500

    Added summary to about component

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ function About(){
     return(
         <div className='about-container'>
             <span className='about-content'>
-                Here is a sentence as filler! From the about page.
+                Full stack developer with background in education and design. Experienced in Javascript and associated libraries, React and Redux. Passionate about providing smooth user experience while easing developer experience, with tools and practices to promote clean architectural design and organization.
             </span>
         </div>
     )
