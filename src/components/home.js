758edeec798f6151b5a6fe9715a7e27bd63dbe3b
Kevin Lai <kevinlai76@gmail.com>
2020-04-26 22:48:31 -0400

    reconfigured homepage layout

--- src/components/home.js
+++ src/components/home.js
@@ -10,27 +10,27 @@ class Home extends React.Component {
             <div className='body'>
                 <div className="bio-section">
                     <h1>Kevin Lai</h1>
-                    <p>
+                    {/* <p>
                         - brief intro (1-2 senctences)
                         - what do you do?
                             - what are your beiefs
                             - what are you passionate about
                             - what do you think you can contribute
                         - thoughtful ending line
-                    </p>
+                    </p> */}
                 </div>
                 <div className='project-section'>
                     <h1>Projects</h1>
                     <Projects />
                 </div>
-                <div className='about-section'>
+                {/* <div className='about-section'>
                     <h1>About</h1>
                     <About />
                 </div>
                 <div className='contact-section'>
                     <h1>Contact</h1>
                     <Contact />
-                </div>
+                </div> */}
             </div>
         )
     }
